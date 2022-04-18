78158c012d58f43a25f6a4c0de5fe171716ad865
Bilal Bora <bilal.bora@ogr.sakarya.edu.tr>
2022-04-18 23:28:22 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.Name = "Pc";
+            product1.Stock = 80;
+            product1.Description = "Yeni dönem ürün";
+         
+            CartManager cartManager = new CartManager();
+            cartManager.Add(product1);
+        }
+    }
+}
